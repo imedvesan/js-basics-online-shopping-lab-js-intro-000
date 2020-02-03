6713adfe6bc355fdb4eca602f4b250fe69b07e2b
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-03 19:49:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var itemPrice = Math.floor(Math.random()*101);
- Object.assign({}, cart, {itemName:'item',itemPrice:'itemPrice'});
- return `${itemName} has been added to your cart.`;
+ var price = Math.floor(Math.random()*100) + 1;
+ var item_object = {};
+  item_object[item] = price
+  cart.push(item_object)
+  console.log(item + " has been added to your cart.");
+  return cart;
 }
 
 function viewCart() {
