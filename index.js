d9e5739b3fb006a87f0e4b63cd22bf5fc5724791
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-03 20:14:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ function getCart() {
 function setCart(c) {
   cart = c;
   return cart;
-}
 
 function addToCart(item) {
  // write your code here
  var price = Math.floor(Math.random()*100) + 1;
- cart.push({[item]:price});
+ cart.push({[itemName]:item,[itemPrice]:price});
   console.log(item + " has been added to your cart.");
   return cart;
 }
